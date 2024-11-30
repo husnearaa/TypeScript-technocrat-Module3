8bd5eb31b2de341e07314198b07d099ac1e9bfc2
husneara <husnearaa99@gmail.com>
2024-11-30 17:28:52 +0600

    has learned the instanceof guard by the instanceof guard is check data oype of guard

--- module3/dist/3.3.js
+++ module3/dist/3.3.js
@@ -1,1 +1,30 @@
 "use strict";
+{
+    var add = function (param1, param2) {
+        if (typeof param1 === "number" && typeof param2 === "number") {
+            return param1 + param2;
+        }
+        else {
+            return param1.toString() + param2.toString();
+        }
+    };
+    var result1 = add("2", "3");
+    console.log(result1);
+    var getUser = function (user) {
+        if ("role" in user) {
+            console.log("My name is ".concat(user.name, " and my role is ").concat(user.role));
+        }
+        else {
+            console.log("My name is ".concat(user.name));
+        }
+    };
+    var normalUser = {
+        name: "Mr. Normal Bhai",
+    };
+    var adminUser = {
+        name: "Mr. Admin Bhai",
+        role: "admin",
+    };
+    getUser(adminUser);
+    //
+}
